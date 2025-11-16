e055c666f8f2e8550190d3db0a58f2e29033f30e
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2025-11-16 11:58:19 -0500

    Update ExtractRequests.java

--- ExtractRequests.java
+++ ExtractRequests.java
@@ -1,2 +1,15 @@
-<!-- Remove ANY empty element before JSON conversion -->
-<xsl:template match="[not() and not(@*) and not(normalize-space())]" />
+<xsl:when test="not(*) and not(@*)">
+    <xsl:choose>
+        <!-- If the element is empty or only whitespace -> JSON null -->
+        <xsl:when test="normalize-space() = ''">
+            <xsl:text>null</xsl:text>
+        </xsl:when>
+
+        <!-- Otherwise output the string value as before -->
+        <xsl:otherwise>
+            <xsl:text>"</xsl:text>
+            <xsl:value-of select="normalize-space()"/>
+            <xsl:text>"</xsl:text>
+        </xsl:otherwise>
+    </xsl:choose>
+</xsl:when>
