547143d9eb07e217b54983e216322329ffffd1f8
Raj Reddy <104778354+rajekin@users.noreply.github.com>
2026-02-13 12:27:15 -0500

    Update ExtractRequests.java

--- ExtractRequests.java
+++ ExtractRequests.java
@@ -1,54 +1,94 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<xsd:schema
-        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-        targetNamespace="http://www.example.org/bom"
-        xmlns="http://www.example.org/bom"
-        elementFormDefault="qualified">
+import com.sun.xml.xsom.*;
+import com.sun.xml.xsom.parser.XSOMParser;
 
-    <!-- ROOT -->
-    <xsd:element name="collections">
-        <xsd:complexType>
-            <xsd:sequence>
-                <xsd:element name="request" type="Request"/>
-                <xsd:element name="response" type="Response" minOccurs="0"/>
-            </xsd:sequence>
-        </xsd:complexType>
-    </xsd:element>
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
-    <!-- REQUEST -->
-    <xsd:complexType name="Request">
-        <xsd:sequence>
-            <xsd:element name="customerData" type="CustomerData"/>
-        </xsd:sequence>
-    </xsd:complexType>
+public class XsdXpathExtractor {
 
-    <!-- RESPONSE (empty for now) -->
-    <xsd:complexType name="Response">
-        <xsd:sequence/>
-    </xsd:complexType>
+    public static class Xpath {
+        private String objectName;
+        private String xpath;
 
-    <!-- YOUR EXACT FIELDS (from screenshot) -->
-    <xsd:complexType name="CustomerData">
-        <xsd:sequence>
-            <xsd:element name="accountNumb" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="memberNumber" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="zip" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="accountBal" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="loanPurpose" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="loanPrincAmount" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="daysSinceFirstTrade" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="numTradesWorstRating60DaysPastDueWithin6Months" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="fico09Score" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="daysSinceLastDerog" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="loanTerm" type="xsd:int" minOccurs="0"/>
-            <xsd:element name="referredLang" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="referredFlag" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="dealerFlag" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="newUsed" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="category3" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="category4" type="xsd:string" minOccurs="0"/>
-            <xsd:element name="actionType" type="xsd:string" minOccurs="0"/>
-        </xsd:sequence>
-    </xsd:complexType>
+        public Xpath(String objectName, String xpath) {
+            this.objectName = objectName;
+            this.xpath = xpath;
+        }
 
-</xsd:schema>
+        @Override
+        public String toString() {
+            return objectName + " -> " + xpath;
+        }
+    }
+
+    private static Set<String> visitedTypes = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+
+        File xsdFile = new File("schema.xsd");
+
+        XSOMParser parser = new XSOMParser();
+        parser.parse(xsdFile);
+
+        XSSchemaSet schemaSet = parser.getResult();
+
+        for (XSSchema schema : schemaSet.getSchemas()) {
+            for (XSElementDecl element : schema.getElementDecls().values()) {
+                processElement(element, "");
+            }
+        }
+    }
+
+    private static void processElement(XSElementDecl element, String parentPath) {
+
+        String currentPath = parentPath + "/" + element.getName();
+
+        XSType type = element.getType();
+
+        if (type.isComplexType()) {
+            processComplexType(type.asComplexType(), currentPath);
+        }
+    }
+
+    private static void processComplexType(XSComplexType complexType, String parentPath) {
+
+        if (visitedTypes.contains(complexType.getName())) return;
+        visitedTypes.add(complexType.getName());
+
+        // Handle attributes
+        for (XSAttributeUse attrUse : complexType.getAttributeUses()) {
+            XSAttributeDecl attr = attrUse.getDecl();
+            System.out.println(new Xpath(attr.getName(), parentPath + "/@" + attr.getName()));
+        }
+
+        // Handle content model (sequence, choice, all)
+        XSParticle particle = complexType.getContentType().asParticle();
+        if (particle != null) {
+            processParticle(particle, parentPath);
+        }
+
+        // Handle extension / inheritance
+        XSType baseType = complexType.getBaseType();
+        if (baseType != null && baseType.isComplexType()) {
+            processComplexType(baseType.asComplexType(), parentPath);
+        }
+    }
+
+    private static void processParticle(XSParticle particle, String parentPath) {
+
+        XSTerm term = particle.getTerm();
+
+        if (term.isElementDecl()) {
+            XSElementDecl child = term.asElementDecl();
+            processElement(child, parentPath);
+        }
+
+        else if (term.isModelGroup()) {
+            XSModelGroup group = term.asModelGroup();
+            for (XSParticle childParticle : group.getChildren()) {
+                processParticle(childParticle, parentPath);
+            }
+        }
+    }
+}
